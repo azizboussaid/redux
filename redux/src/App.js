82163e5f15a008ad94946a7b3e1d925b4d171bde
aziz boussaid <azizboussaied062@gmail.com>
2023-11-16 09:34:00 +0100

    redux

---
+++ redux/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Form from './From';
+import List from './Liste';
+
+
+
+function App() {
+  return (
+    <div className='App'>
+    <Form />
+    <List />
+    </div>
+  );
+}
+
+export default App;
